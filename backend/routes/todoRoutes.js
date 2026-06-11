29be21cbaa2cd4140c69c1a43d59f1d4e5380c8a
Taher <taher.najim@yahoo.com>
2026-06-11 19:19:36 +0800

    added get route for fetching one todo

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -21,16 +21,27 @@ router.post("/", async (req, res) => {
     res.status(201).json(newTodo);
   } catch (error) {
     console.error("Error creating todo:", error);
-    res.status(500).json({ message: "Could not create todo", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Could not create todo", error: error.message });
   }
 });
 
 // Read data for a todo (maybe use for prefilling edit form)
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const todoId = req.params.id;
-  // get that todo from database
-  // res.json(todo) from the database
-  res.send("display a single todo with the id: " + todoId);
+  try {
+    const todo = await Todo.findById(todoId);
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ message: `Todo with ID ${todoId} not found` });
+    }
+    res.status(200).json(todo);
+  } catch (error) {
+    console.error("Error fetching todo:", error);
+    res.status(500).json({ message: `Could not fetch todo id no: ${todoId}` });
+  }
 });
 
 // Update todo
